fac0f805e758ba3836163b070c4695272b5d35cc
Bruk Lemma <bruklemma2017@gmail.com>
2022-05-19 16:34:11 +0300

    update our manu-item to navigate withRouter HOC higher order component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import { Route,Switch,Link } from 'react-router-dom';
+
+
+const HatsPage=(props)=>(
+  <div>
+  
+    <h1>Hats page</h1>
+    </div>
+);
 
 function App() {
   return (
-    <div> <HomePage/> </div>
+    <div> 
+      <Switch>
+      <Route  exact path='/'     component={HomePage}/>
+      <Route  path='/hats' component={HatsPage}/> 
+      </Switch>
+    </div>
   );
 }
-//this is a test script
+
 export default App;
